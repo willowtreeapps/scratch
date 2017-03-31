c004c8fc0437c1936637189a2f4eed99c721051f
Eric Richardson <eric.richardson97@gmail.com>
2017-03-31 15:29:15 -0400

    Initial commit

---
+++ scratchtileapp/src/main/java/com/willowtreeapps/scratch/ScratchTileService.kt
@@ -0,0 +1,24 @@
+package com.willowtreeapps.scratch
+
+import android.content.Intent
+import android.service.quicksettings.TileService
+import android.util.Log
+
+class ScratchTileService : TileService() {
+    override fun onClick() {
+        sendBroadcast(Intent(getString(R.string.scratch_intent)))
+        Log.d("Scratch", "Scratch Broadcast Sent")
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+}
