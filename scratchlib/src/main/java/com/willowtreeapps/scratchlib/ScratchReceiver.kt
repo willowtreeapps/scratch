0a6b23f01df610fe17edfc91d56d5172acd218e3
Eric Richardson <eric.richardson97@gmail.com>
2017-03-31 16:38:53 -0400

    Added restart for foreground only application.

--- scratchlib/src/main/java/com/willowtreeapps/scratchlib/ScratchReceiver.kt
+++ scratchlib/src/main/java/com/willowtreeapps/scratchlib/ScratchReceiver.kt
@@ -1,20 +1,42 @@
 package com.willowtreeapps.scratchlib
 
+import android.app.ActivityManager
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.content.Context.ACTIVITY_SERVICE
-import android.app.ActivityManager
+import android.content.Intent
 import android.util.Log
 
 
 class ScratchReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("Scratch", "Scratch Broadcast Received")
-        val status = (context?.getSystemService(ACTIVITY_SERVICE) as ActivityManager)
-                .clearApplicationUserData()
-        Log.d("Scratch", "Scratch cleared data: " + status)
+        if (isInForeground()) {
+            restartAfterClean(context)
+            Log.d("Scratch", "Scratch Broadcast Received")
+            val status = (context?.getSystemService(ACTIVITY_SERVICE) as ActivityManager)
+                    .clearApplicationUserData()
+            Log.d("Scratch", "Scratch cleared data: " + status)
+        }
+    }
+
+    private fun restartAfterClean(context: Context?) {
+        val intent = getLaunchActivity(context)
+        val restartBroadcastIntent = Intent("com.willowtreeapps.scratch.RESTART")
+        restartBroadcastIntent.putExtra("restartIntent", intent)
+        context?.sendBroadcast(restartBroadcastIntent)
+    }
+
+    private fun getLaunchActivity(context: Context?): Intent? {
+        val pm = context?.packageManager
+        return pm?.getLaunchIntentForPackage(context?.packageName)
+    }
+
+    fun isInForeground(): Boolean {
+        val process = ActivityManager.RunningAppProcessInfo()
+        ActivityManager.getMyMemoryState(process)
+        return process.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
     }
 }
+
 
 
